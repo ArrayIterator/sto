fe2b7d05697172287352e0cc252527213890c49f
arrayiterator <arrayiterator@gmail.com>
2020-12-07 08:13:47 +0700

    add modules getter & setter

--- lib/functions/options.php
+++ lib/functions/options.php
@@ -6,12 +6,29 @@ use ArrayIterator\ArrayGetter;
  * @param string $name
  * @param null $default
  * @param int|null $siteId
- * @param bool $usePrevious
+ * @param $found
  * @return mixed
  */
-function get_option(string $name, $default = null, int $siteId = null, bool $usePrevious = true)
+function get_option(string $name, $default = null, int $siteId = null, &$found = null)
 {
-    $data = option()->value($name, $default, $siteId, $usePrevious);
+    $cacheSiteId = cache_get_site_id();
+    $siteId = $siteId === null ? get_current_site_id() : $siteId;
+    $switched = $cacheSiteId !== $siteId;
+    if ($switched) {
+        cache_switch_to($siteId);
+    }
+    $cache = cache_get($name, 'site_options', $found);
+    if ($found) {
+        $switched && cache_switch_to($cacheSiteId);
+        return hook_apply('options_' . $name, $cache, $name, $default, $siteId);
+    }
+
+    $data = option()->value($name, $default, $siteId, $found);
+    if ($found) {
+        cache_set($name, $data, 'site_options');
+    }
+
+    $switched && cache_switch_to($cacheSiteId);
     return hook_apply('options_' . $name, $data, $name, $default, $siteId);
 }
 
@@ -23,6 +40,14 @@ function get_option(string $name, $default = null, int $siteId = null, bool $use
  */
 function update_option(string $name, $value, int $siteId = null): bool
 {
+    $cacheSiteId = cache_get_site_id();
+    $siteId = $siteId === null ? get_current_site_id() : $siteId;
+    $switched = $cacheSiteId !== $siteId;
+    if ($switched) {
+        cache_switch_to($siteId);
+    }
+    cache_set($name, $value, 'site_options');
+    $switched && cache_switch_to($cacheSiteId);
     return option()->set($name, $value, $siteId);
 }
 
@@ -46,6 +71,14 @@ function update_options(array $value, int $siteId = null): bool
     if (empty($value)) {
         return false;
     }
+
+    $cacheSiteId = cache_get_site_id();
+    $siteId = $siteId === null ? get_current_site_id() : $siteId;
+    $switched = $cacheSiteId !== $siteId;
+    if ($switched) {
+        cache_switch_to($siteId);
+    }
+
     $options = option();
     $value = [];
     foreach ($value as $key => $item) {
@@ -53,8 +86,11 @@ function update_options(array $value, int $siteId = null): bool
         if (!is_string($key)) {
             continue;
         }
+        cache_set($key, $item, 'site_options');
         $value[$key] = $options->sanitizeDatabaseValue('option_value', $item);
     }
+
+    $switched && cache_switch_to($cacheSiteId);
     if (empty($value)) {
         return false;
     }
@@ -105,6 +141,17 @@ function delete_option(string $name, int $siteId = null): bool
         $options->getTableName(),
         ($siteId === null ? ' IS NULL' : sprintf('=%s', $siteId))
     );
+
+    $cacheSiteId = cache_get_site_id();
+    $siteId = $siteId === null ? get_current_site_id() : $siteId;
+    $switched = $cacheSiteId !== $siteId;
+    if ($switched) {
+        cache_switch_to($siteId);
+    }
+
+    cache_delete($name, 'site_options');
+    $switched && cache_switch_to($cacheSiteId);
+
     $stmt = database_prepare($sql);
     $res = $stmt->execute([$name]);
     $stmt->closeCursor();
@@ -121,16 +168,26 @@ function delete_options(array $name, int $siteId = null): bool
     if (empty($name)) {
         return false;
     }
+    $cacheSiteId = cache_get_site_id();
+    $siteId = $siteId === null ? get_current_site_id() : $siteId;
+    $switched = $cacheSiteId !== $siteId;
+    if ($switched) {
+        cache_switch_to($siteId);
+    }
     foreach ($name as $key => $item) {
         unset($name[$key]);
         if (!is_string($item)) {
             continue;
         }
+        cache_delete($item, 'site_options');
         $name[$item] = '?';
     }
+
+    $switched && cache_switch_to($cacheSiteId);
     if (empty($name)) {
         return false;
     }
+
 
     $options = option();
     $siteId = func_num_args() === 1
@@ -150,4 +207,146 @@ function delete_options(array $name, int $siteId = null): bool
     $res = $stmt->execute(array_keys($name));
     $stmt->closeCursor();
     return (bool)$res;
+}
+
+function get_site_option(string $optionName, $default = null)
+{
+    $cache = cache_get($optionName, 'site_options', $found);
+    if ($found) {
+        return $cache;
+    }
+
+    $res = get_option($optionName, $default, get_current_site_id(), $found);
+    if ($found) {
+        cache_set($optionName, $res);
+    }
+    return $res;
+}
+
+function get_site_wide_active_modules(): array
+{
+    $modules = get_option('active_modules', null, 1);
+    $update = false;
+    if (!is_array($modules)) {
+        $modules = [];
+    }
+    foreach ($modules as $key => $time) {
+        if (!is_string($key) || !is_int($time)) {
+            $update = true;
+            unset($modules[$key]);
+        }
+    }
+
+    if ($update) {
+        update_option('active_modules', $modules, 1);
+    }
+
+    asort($modules);
+    return hook_apply('site_wide_active_modules', $modules);
+}
+
+/**
+ * @return array
+ */
+function get_site_active_modules(): array
+{
+    $modules = get_site_option('active_modules');
+
+    if (!is_array($modules)) {
+        $modules = [];
+    }
+
+    $update = false;
+    if (!is_array($modules)) {
+        $modules = [];
+    }
+
+    foreach ($modules as $key => $time) {
+        if (!is_string($key) || !is_int($time)) {
+            $update = true;
+            unset($modules[$key]);
+        }
+    }
+
+    if ($update) {
+        update_option('active_modules', $modules, get_current_site_id());
+    }
+    asort($modules);
+    return hook_apply('site_active_modules', $modules);
+}
+
+/**
+ * @param string $module
+ * @param bool $replaceTime
+ * @return bool
+ */
+function set_site_wide_active_module(string $module, bool $replaceTime = false): bool
+{
+    $modules = get_site_wide_active_modules();
+    if (isset($modules[$module]) && !$replaceTime) {
+        return true;
+    }
+    $modules[$module] = time();
+    return update_option('active_modules', $modules, 1);
+}
+
+/**
+ * @param string $module
+ * @param bool $replaceTime
+ * @return bool
+ */
+function set_site_active_module(string $module, bool $replaceTime = false): bool
+{
+    $modules = get_site_active_modules();
+    if (isset($modules[$module]) && !$replaceTime) {
+        return true;
+    }
+
+    $modules[$module] = time();
+    return update_option('active_modules', $modules);
+}
+
+/**
+ * @param string $module
+ * @return bool
+ */
+function remove_site_wide_active_module(string $module): bool
+{
+    $modules = get_site_wide_active_modules();
+    if (!isset($modules[$module])) {
+        return false;
+    }
+    unset($modules[$module]);
+    return update_option('active_modules', $modules, 1);
+}
+
+/**
+ * @param string $module
+ * @return bool
+ */
+function remove_site_active_module(string $module): bool
+{
+    $modules = get_site_active_modules();
+    if (!isset($modules[$module])) {
+        return false;
+    }
+    unset($modules[$module]);
+    return update_option('active_modules', $modules);
+}
+
+/**
+ * @return array
+ */
+function get_all_active_modules(): array
+{
+    $modules = get_site_wide_active_modules();
+    $siteModules = get_site_active_modules();
+    foreach ($siteModules as $key => $item) {
+        if (!isset($modules[$key])) {
+            $modules[$key] = $item;
+        }
+    }
+
+    asort($modules);
+    return $modules;
 }
